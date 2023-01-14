68fd351dfbe8e9090a34e154a5c0165d089f808a
peter92q <114609817+peter92q@users.noreply.github.com>
2023-01-14 08:36:21 +0200

    Update index.js

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,16 +1,12 @@
 import "./style.css";
 import { Link } from "react-router-dom";
 import {
-  ArrowDown,
   Friends,
   FriendsActive,
   Gaming,
   Home,
   HomeActive,
-  Logo,
   Market,
-  Menu,
-  Messenger,
   Notifications,
   Search,
   Watch,
@@ -18,7 +14,7 @@ import {
 import { useSelector } from "react-redux";
 import SearchMenu from "./SearchMenu";
 import { useRef, useState } from "react";
-import AllMenu from "./AllMenu";
+
 import useClickOutside from "../../helpers/clickOutside";
 import UserMenu from "./userMenu";
 export default function Header({ page, getAllPosts }) {
@@ -100,25 +96,7 @@ export default function Header({ page, getAllPosts }) {
           <img src={user?.picture} alt="" />
           <span>{user?.first_name}</span>
         </Link>
-        <div
-          className={`circle_icon hover1 ${showAllMenu && "active_header"}`}
-          ref={allmenu}
-        >
-          <div
-            onClick={() => {
-              setShowAllMenu((prev) => !prev);
-            }}
-          >
-            <div style={{ transform: "translateY(2px)" }}>
-              <Menu />
-            </div>
-          </div>
-
-          {showAllMenu && <AllMenu />}
-        </div>
-        <div className="circle_icon hover1">
-          <Messenger />
-        </div>
+        
         <div className="circle_icon hover1">
           <Notifications />
           <div className="right_notification">5</div>
@@ -132,11 +110,7 @@ export default function Header({ page, getAllPosts }) {
               setShowUserMenu((prev) => !prev);
             }}
           >
-            <div style={{ transform: "translateY(2px)" }}>
-              <ArrowDown />
-            </div>
           </div>
-
           {showUserMenu && <UserMenu user={user} />}
         </div>
       </div>
