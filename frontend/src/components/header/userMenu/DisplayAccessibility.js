9a1b2b31c1771c9b117bda340fc5e55a107071d5
peter92q <114609817+peter92q@users.noreply.github.com>
2023-01-14 08:49:29 +0200

    Update DisplayAccessibility.js

--- frontend/src/components/header/userMenu/DisplayAccessibility.js
+++ frontend/src/components/header/userMenu/DisplayAccessibility.js
@@ -11,8 +11,7 @@ export default function DisplayAccessibility({ setVisible }) {
           className="circle hover1"
           onClick={() => {
             setVisible(0);
-          }}
-        >
+          }}>
           <i className="arrow_back_icon"></i>
         </div>
         Display & Accessibility
@@ -34,9 +33,7 @@ export default function DisplayAccessibility({ setVisible }) {
         className="hover1"
         onClick={() => {
           Cookies.set("darkTheme", false);
-          dispatch({ type: "LIGHT" });
-        }}
-      >
+          dispatch({ type: "LIGHT" });}}>
         <span>Off</span>
         {darkTheme ? (
           <input type="radio" name="dark" id="darkOff" />
@@ -49,10 +46,7 @@ export default function DisplayAccessibility({ setVisible }) {
         className="hover1"
         onClick={() => {
           Cookies.set("darkTheme", true);
-
-          dispatch({ type: "DARK" });
-        }}
-      >
+          dispatch({ type: "DARK" });}}>
         <span>On</span>
         {darkTheme ? (
           <input type="radio" name="dark" id="darkOn" checked />
@@ -60,33 +54,7 @@ export default function DisplayAccessibility({ setVisible }) {
           <input type="radio" name="dark" id="darkOn" />
         )}
       </label>
-      <div className="mmenu_main">
-        <div className="small_circle" style={{ width: "50px" }}>
-          <i className="compact_icon"></i>
-        </div>
-        <div className="mmenu_col">
-          <span className="mmenu_span1">Compact mode</span>
-          <span className="mmenu_span2">
-            Make your font size smaller so more content can fit on the screen.
-          </span>
-        </div>
-      </div>
-      <label htmlFor="compactOff" className="hover1">
-        <span>Off</span>
-        <input type="radio" name="compact" id="compactOff" />
-      </label>
-      <label htmlFor="compactOn" className="hover1">
-        <span>On</span>
-        <input type="radio" name="compact" id="compactOn" />
-      </label>
       <div className="mmenu_item hover3">
-        <div className="small_circle">
-          <i className="keyboard_icon"></i>
-        </div>
-        <span>Keyboard</span>
-        <div className="rArrow">
-          <i className="right_icon"></i>
-        </div>
       </div>
     </div>
   );
